dbee70e62c2206c17012f29ad92d1c977b24d61f
Havrylyuk-UA <142251279+Havrylyuk-UA@users.noreply.github.com>
2024-02-09 15:32:21 +0200

    test

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,12 +1,17 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { selectVisibleContacts } from '../../redux/selectors';
 import { deleteContact } from '../../redux/operations';
+import { useEffect } from 'react';
 
 import './ContactList.css';
 import ContactListItem from '../ContactListItem/ContactListItem';
 
 const ContactList = () => {
   const contacts = useSelector(selectVisibleContacts);
+
+  useEffect(() => {
+    console.log(contacts);
+  }, [contacts]);
 
   const dispatch = useDispatch();
 
